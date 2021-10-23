5d0307a0dcc5c68810687316f8936a57e92ad5ff
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-10-23 23:25:59 +0200

    p11. integration test test reset db fixed. Test fails

--- supersub/tests/integration/management/commands/test_reset_db.py
+++ supersub/tests/integration/management/commands/test_reset_db.py
@@ -9,6 +9,8 @@ from pur_beurre.custom_settings.tests_variables import (
 from supersub.management.commands.reset_db import Command
 from supersub.models.category import Category
 from supersub.models.product import Product
+from supersub.tests.unit.models.test_category import CategoryTest
+from supersub.tests.unit.models.test_product import ProductTest
 
 
 class CommandTest(TestCase):
@@ -19,41 +21,22 @@ class CommandTest(TestCase):
     def setUpTestData(cls):
         """Method that setup data for the whole class
         """
-        cls.emulate_off_api_manager_categories()
-        cls.emulate_off_api_manager_products()
         cls.db_manager = Command()
+        CategoryTest.emulate_category()
+        ProductTest.emulate_product()
 
-    @classmethod
-    def emulate_off_api_manager_categories(cls):
-        """Emulation of off api manager categories attribute. It consist of
-        valid categories that have been filtered out from api response
-        to ensureit suits db requirements.
-        """
-        cls.categories = OFF_API_FILTERED_CATEGORIES
-
-    @classmethod
-    def emulate_off_api_manager_products(cls):
-        """Emulation of off api manager products attribute. It consist of
-        valid products that have been filtered out from api response to ensure
-        it suits db requirements
-        """
-        cls.products = OFF_API_FILTERED_PRODUCTS
-    
     def test_reset_db_with_handle_command_execution(self):
         init_category = Category.objects.order_by('id')[0]
         init_product = Product.objects.order_by('id')[0]
-        self.db_manager.handle()
+        options = {'all': False}
+        self.db_manager.handle(**options)
         post_category = Category.objects.order_by('id')[0]
         post_product = Product.objects.order_by('id')[0]
         category_message = "Category is not reset"
         product_message = "Product is not reset"
-        self.assertTrue(init_category)
-        self.assertTrue(init_product)
-        self.assertTrue(post_category)
-        self.assertTrue(post_product)
         self.assertNotEqual(
-            init_category.id, post_category.id, category_message
+            init_category.name, post_category.name, category_message
         )
         self.assertNotEqual(
-            init_product.id, post_product.id, product_message
+            init_product.name, post_product.name, product_message
         )
